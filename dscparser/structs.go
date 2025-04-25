b47740f2defd9d80c3cfc62b4b75e4ff7cf7da75
Max Klebe <klebe@denic.de>
2025-04-25 13:20:36 +0200

    Changed equal function of dscdata and other structs to dont use reflect.deepequal anymore

--- dscparser/structs.go
+++ dscparser/structs.go
@@ -3,7 +3,6 @@ package dscparser
 import (
 	"cmp"
 	"encoding/xml"
-	"reflect"
 	"slices"
 )
 
@@ -23,16 +22,23 @@ func (d *DSCData) Sort() {
 	slices.SortFunc(d.Datasets, cmpDataset)
 
 	//Sort rows and cells
-	for i := range len(d.Datasets) {
-		dataset := d.Datasets[i]
+	for _, dataset := range d.Datasets {
 		dataset.Sort()
 	}
 }
 
 func (d DSCData) Equals(d2 DSCData) bool {
+	if len(d.Datasets) != len(d2.Datasets) || d.NameServer != d2.NameServer || d.Location != d2.Location {
+		return false
+	}
 	d.Sort()
 	d2.Sort()
-	return reflect.DeepEqual(d, d2)
+	for i := range d.Datasets {
+		if !d.Datasets[i].Equals(d2.Datasets[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 type Dataset struct {
@@ -49,24 +55,34 @@ func (ds *Dataset) Sort() {
 	cmpRow := func(a, b Row) int {
 		return cmp.Compare(a.Value, b.Value)
 	}
-	cmpCell := func(a, b Cell) int {
-		return cmp.Compare(a.Value, b.Value)
-	}
-
 	//Sort rows
 	slices.SortFunc(ds.Data.Rows, cmpRow)
 
 	//Sort cells
-	for i := range len(ds.Data.Rows) {
-		ds1Row := ds.Data.Rows[i]
-		slices.SortFunc(ds1Row.Cells, cmpCell)
+	for _, row := range ds.Data.Rows {
+		row.Sort()
 	}
 }
 
 func (ds Dataset) Equals(ds2 Dataset) bool {
+	if len(ds.DimensionInfo) != len(ds2.DimensionInfo) || ds.Dimensions != ds2.Dimensions ||
+		ds.StartTime != ds2.StartTime || ds.StopTime != ds2.StopTime ||
+		ds.Name != ds2.Name {
+		return false
+	}
+	for i := range ds.DimensionInfo {
+		if ds.DimensionInfo[i].Number != ds2.DimensionInfo[i].Number || ds.DimensionInfo[i].Type != ds2.DimensionInfo[i].Type {
+			return false
+		}
+	}
 	ds.Sort()
 	ds2.Sort()
-	return reflect.DeepEqual(ds, ds2)
+	for i := range ds.Data.Rows {
+		if !ds.Data.Rows[i].Equals(ds2.Data.Rows[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 type DimensionInfo struct {
@@ -85,8 +101,33 @@ type Row struct {
 	Cells   []Cell `xml:",any"`
 }
 
+func (r *Row) Sort() {
+	cmpCell := func(a, b Cell) int {
+		return cmp.Compare(a.Value, b.Value)
+	}
+	slices.SortFunc(r.Cells, cmpCell)
+}
+
+func (r Row) Equals(r2 Row) bool {
+	if len(r.Cells) != len(r2.Cells) || r.XMLName.Local != r2.XMLName.Local || r.Value != r2.Value {
+		return false
+	}
+	r.Sort()
+	r2.Sort()
+	for i := range r.Cells {
+		if !r.Cells[i].Equals(r2.Cells[i]) {
+			return false
+		}
+	}
+	return true
+}
+
 type Cell struct {
 	XMLName xml.Name
 	Value   string `xml:"val,attr"`
 	Count   int    `xml:"count,attr"`
+}
+
+func (c Cell) Equals(c2 Cell) bool {
+	return c.XMLName.Local == c2.XMLName.Local && c.Value == c2.Value && c.Count == c2.Count
 }
