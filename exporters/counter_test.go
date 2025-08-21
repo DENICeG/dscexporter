da07f72f8cf27ddb1c6ead76818eff34471129ce
Max Klebe <klebe@denic.de>
2025-08-21 16:37:58 +0200

    Use template struct for custom metrics

--- exporters/counter_test.go
+++ exporters/counter_test.go
@@ -1,6 +1,104 @@
 package exporters
 
-// func TestAddToCounterHistory(t *testing.T) {
-// 	counterValueHistory := CounterValueHistory{}
-// 	counterValueHistory.
-// }
+import (
+	"testing"
+	"time"
+
+	"github.com/DENICeG/dscexporter/config"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+)
+
+func prepare() (counterVec *CounterVec, labelValues LabelValues) {
+	desc := prometheus.NewDesc(
+		"metric",
+		"help",
+		[]string{LOCATION_LABEL, NAMESERVER_LABEL},
+		nil,
+	)
+	counterVec = NewCounterVec(desc)
+
+	labelValues = LabelValues{
+		Location:   "loc",
+		Nameserver: "ns",
+	}
+	return
+}
+
+func collectValues(ch chan prometheus.Metric) []prometheus.Metric {
+	metrics := make([]prometheus.Metric, 0)
+	for metric := range ch {
+		metrics = append(metrics, metric)
+	}
+	return metrics
+}
+
+func TestAddToCounterHistory(t *testing.T) {
+	SetConf(&config.Config{
+		Prometheus: config.PrometheusConfig{
+			Timestamps: true,
+			WindowSize: 3,
+		},
+	})
+	counterVec, labelValues := prepare()
+
+	now := time.Now()
+	counterVec.Add(labelValues, 10, now.Add(-3*time.Minute))
+	counterVec.Add(labelValues, 10, now.Add(-2*time.Minute))
+	counterVec.Add(labelValues, 10, now.Add(-1*time.Minute))
+	counterVec.Add(labelValues, 10, now)
+
+	history := counterVec.withLabelValues(labelValues)
+	assert.Equal(t,
+		[]CounterValue{{
+			Value:     40,
+			Timestamp: now,
+		}, {
+			Value:     30,
+			Timestamp: now.Add(-1 * time.Minute),
+		}, {
+			Value:     20,
+			Timestamp: now.Add(-2 * time.Minute),
+		}},
+		history.Values,
+	)
+}
+
+func TestCollect(t *testing.T) {
+	SetConf(&config.Config{
+		Prometheus: config.PrometheusConfig{
+			Timestamps: true,
+			WindowSize: 3,
+		},
+	})
+	counterVec, labelValues := prepare()
+	now := time.Now()
+	counterVec.Add(labelValues, 10, now.Add(-1*time.Minute))
+	counterVec.Add(labelValues, 10, now)
+
+	ch := make(chan prometheus.Metric)
+	go counterVec.Collect(ch)
+
+	metrics := collectValues(ch)
+	assert.Equal(t, 2, metrics)
+}
+
+func TestCollectToOld(t *testing.T) {
+	SetConf(&config.Config{
+		Prometheus: config.PrometheusConfig{
+			Timestamps: false,
+			WindowSize: 3,
+		},
+	})
+
+	counterVec, labelValues := prepare()
+	now := time.Now()
+	counterVec.Add(labelValues, 10, now.Add(-4*time.Minute)) // Metric older than 3 Minutes, so its not collected
+	counterVec.Add(labelValues, 10, now.Add(-2*time.Minute))
+
+	ch := make(chan prometheus.Metric)
+	go counterVec.Collect(ch)
+
+	metrics := collectValues(ch)
+	assert.Equal(t, 1, metrics)
+}
